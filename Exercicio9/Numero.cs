a150b52629e0947fceb903b7b46eeda09fbe8547
Ronaldo <ronaldolucas_gc@hotmail.com>
2019-05-11 20:48:02 -0300

    Initial commit

---
+++ Exercicio9/Numero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio9
+{
+    public class Numero
+    {
+        public static void Main()
+        { 
+
+        }
+        public static bool EhPar(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Exercicio9Tests1/NumeroTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercicio9;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio9.Tests
+{
+    [TestClass()]
+    public class NumeroTests
+    {
+        [TestMethod()]
+        public void EhParTest()
+        {
+            bool resultado = Numero.EhPar(2);
+
+            Assert.IsTrue(resultado);
+        }
+
+        [TestMethod()]
+        public void EhImparTest()
+        {
+            bool resultado = Numero.EhPar(3);
+
+            Assert.IsFalse(resultado);
+        }
+    }
+}
